67f1ca079fe48bf1a507aa991585f3f5c9320589
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-26 13:26:56 -0700

    constant header position

--- src/pages/create_entry.tsx
+++ src/pages/create_entry.tsx
@@ -8,9 +8,9 @@ export const CreateEntry = () =>{
   const [user] = useAuthState(auth)
   user && console.log(`Logged in as  ${user?.displayName}`)
     return (
-        <div> 
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
           <Header />
           <Entry />
-        </div>
+      </div>
     )
 }
